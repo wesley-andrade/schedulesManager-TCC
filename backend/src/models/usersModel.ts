9bfb4a168bf9575f2f0658ae6bb250c2630c1a1f
Wesley Andrade <wesleyandrade1217@gmail.com>
2025-03-04 21:10:01 -0300

    Adicionando usersModel com operações CRUD para gerenciamento de usuários

---
+++ backend/src/models/usersModel.ts
@@ -0,0 +1,60 @@
+import { users } from "../data/mockData";
+import { Role, User } from "../types";
+import bcrypt from "bcrypt";
+
+const getAllUsers = (): Omit<User, "password">[] => {
+  return users.map(({ password, ...user }) => user);
+};
+
+const getUserById = (id: number): User | undefined => {
+  return users.find(user => user.id === id);
+};
+
+const getUserByEmail = (email: string): User | undefined => {
+  return users.find(user => user.email === email);
+};
+
+const createUser = (
+  name: string,
+  email: string,
+  password: string,
+  role: Role
+): User => {
+  const newUser: User = {
+    id: Math.floor(Math.random() * 9999),
+    name,
+    email,
+    password: bcrypt.hashSync(password, 10),
+    role
+  };
+  users.push(newUser);
+  return newUser;
+};
+
+const updateUser = (
+  id: number,
+  updates: Partial<Omit<User, "id">>
+): User | undefined => {
+  const index = users.findIndex(user => user.id === id);
+  if (index === -1) return undefined;
+
+  users[index] = { ...users[index], ...updates };
+  return users[index];
+};
+
+const deleteUser = (id: number): boolean => {
+  const index = users.findIndex(user => user.id === id);
+  if (index === -1) return false;
+
+  users.splice(index, 1);
+  return true;
+};
+
+export default {
+  getAllUsers,
+  getUserById,
+  getUserByEmail,
+  createUser,
+  updateUser,
+  deleteUser
+};
