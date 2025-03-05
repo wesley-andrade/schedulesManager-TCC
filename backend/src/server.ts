288856f685b38a9421343fc150ed96bf094f15ba
Wesley Andrade <wesleyandrade1217@gmail.com>
2025-03-04 21:06:59 -0300

    Modificando server e adicionando nova dependencia

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
 import express from "express";
+import router from "./routes/api";
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
