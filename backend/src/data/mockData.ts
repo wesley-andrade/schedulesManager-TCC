d1dcd1493b025597ebb789fbb8f508b8656a5140
Wesley Andrade <wesleyandrade1217@gmail.com>
2025-04-18 10:06:26 -0300

    Adicionando mais dados mockados e tipos para simular o DB

--- backend/src/data/mockData.ts
+++ backend/src/data/mockData.ts
@@ -1,6 +1,21 @@
 import bcrypt from "bcrypt";
 
-import { User } from "../types";
+import {
+  AcademicPeriod,
+  ClassSchedule,
+  ClassScheduleRoom,
+  Course,
+  Discipline,
+  DisciplineCourse,
+  DisciplineTeacher,
+  Module,
+  Room,
+  Schedule,
+  Teacher,
+  TeacherAvailability,
+  TimeSlot,
+  User,
+} from "../types";
 
 export let users: User[] = [
   {
@@ -8,13 +23,70 @@ export let users: User[] = [
     name: "Dr. João",
     email: "joao@medicina.com",
     password: bcrypt.hashSync("12345678", 10),
-    role: "teacher"
+    role: "teacher",
   },
   {
     id: 2,
     name: "Admin",
     email: "admin@medicina.com",
     password: bcrypt.hashSync("12345678", 10),
-    role: "admin"
-  }
+    role: "admin",
+  },
+];
+
+export let teachers: Teacher[] = [{ id: 1, userId: 1, phone: "99 999999999" }];
+
+export let courses: Course[] = [{ id: 1, name: "Medicina" }];
+
+export let module: Module[] = [{ id: 1, name: "1º Módulo", totalStudants: 60 }];
+
+export let academicPeriod: AcademicPeriod[] = [
+  {
+    id: 1,
+    name: "2023/2",
+    startDate: "2023-08-01",
+    endDate: "2024-01-31",
+  },
+];
+
+export let disciplines: Discipline[] = [
+  {
+    id: 1,
+    name: "Anatomia",
+    moduleId: 1,
+    totalHours: 60,
+    academicPeriodId: 1,
+  },
+];
+
+export let disciplineCourse: DisciplineCourse[] = [
+  { id: 1, disciplineId: 1, courseId: 1 },
+];
+
+export let timeSlots: TimeSlot[] = [
+  { id: 1, startTime: "08:00", endTime: "09:00" },
+];
+
+export let schedules: Schedule[] = [
+  { id: 1, dayOfWeek: "Segunda-feira", timeSlotId: 1 },
+];
+
+export let teacherAvailability: TeacherAvailability[] = [
+  { id: 1, teacherId: 1, status: true, scheduleId: 1 },
+];
+
+export let disciplineTeacher: DisciplineTeacher[] = [
+  { id: 1, disciplineId: 1, teacherId: 1 },
+];
+
+export let room: Room[] = [
+  { id: 1, name: "Sala 101", seatsAmount: 60, type: "Laboratório" },
+];
+
+export let classScheduleRoom: ClassScheduleRoom[] = [
+  { id: 1, classScheduleId: 1, roomId: 1 },
+];
+
+export let classSchedule: ClassSchedule[] = [
+  { id: 1, scheduleId: 1, disciplineTeacherId: 1 },
 ];
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -7,3 +7,85 @@ export interface User {
   password: string;
   role: Role;
 }
+
+export interface Teacher {
+  id: number;
+  userId: number;
+  phone: string;
+}
+
+export interface TeacherAvailability {
+  id: number;
+  teacherId: number;
+  status: boolean;
+  scheduleId: number;
+}
+
+export interface Schedule {
+  id: number;
+  dayOfWeek: string;
+  timeSlotId: number;
+}
+
+export interface TimeSlot {
+  id: number;
+  startTime: string;
+  endTime: string;
+}
+
+export interface ClassSchedule {
+  id: number;
+  scheduleId: number;
+  disciplineTeacherId: number;
+}
+
+export interface DisciplineTeacher {
+  id: number;
+  disciplineId: number;
+  teacherId: number;
+}
+
+export interface Discipline {
+  id: number;
+  name: string;
+  moduleId: number;
+  totalHours: number;
+  academicPeriodId: number;
+}
+
+export interface AcademicPeriod {
+  id: number;
+  name: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface Module {
+  id: number;
+  name: string;
+  totalStudants: number;
+}
+
+export interface DisciplineCourse {
+  id: number;
+  disciplineId: number;
+  courseId: number;
+}
+
+export interface Course {
+  id: number;
+  name: string;
+}
+
+export interface ClassScheduleRoom {
+  id: number;
+  classScheduleId: number;
+  roomId: number;
+}
+
+export interface Room {
+  id: number;
+  name: string;
+  seatsAmount: number;
+  type: string;
+}
