8d116f654d496142c7505777a15a6053ead4952e
Wesley Andrade <wesleyandrade1217@gmail.com>
2025-04-18 22:25:10 -0300

    Adicionando as rotas do timeSlot

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./authRoutes";
 import userRoutes from "./userRoutes";
 import teacherRoutes from "./teacherRoutes";
+import timeSlotsRoutes from "./timeSlotRoutes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/teachers", teacherRoutes);
+router.use("/time-slots", timeSlotsRoutes);
 
 export default router;
---
+++ backend/src/routes/timeSlotRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { authenticate, isAdmin } from "../middlewares/authMiddleware";
+import timeSlotsController from "../controllers/timeSlotsController";
+
+const router = Router();
+
+router.get("/", authenticate, isAdmin, timeSlotsController.index);
+router.get("/:id", authenticate, isAdmin, timeSlotsController.show);
+router.post("/", authenticate, isAdmin, timeSlotsController.create);
+router.put("/:id", authenticate, isAdmin, timeSlotsController.update);
+router.delete("/:id", authenticate, isAdmin, timeSlotsController.delete);
+
+export default router;
