950b2ac87ff4461c81f41d9c090f31eb066093ba
Wesley Andrade <wesleyandrade1217@gmail.com>
2025-04-21 18:37:24 -0300

    Adicionando as rotas de schedules

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "./authRoutes";
 import userRoutes from "./userRoutes";
 import teacherRoutes from "./teacherRoutes";
 import timeSlotsRoutes from "./timeSlotRoutes";
+import scheduleRoutes from "./scheduleRoutes";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/teachers", teacherRoutes);
 router.use("/time-slots", timeSlotsRoutes);
+router.use("/schedules", scheduleRoutes);
 
 export default router;
---
+++ backend/src/routes/scheduleRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { authenticate, isAdmin } from "../middlewares/authMiddleware";
+import scheduleController from "../controllers/scheduleController";
+
+const router = Router();
+
+router.get("/", authenticate, scheduleController.index);
+router.get("/:id", authenticate, scheduleController.show);
+router.post("/", authenticate, isAdmin, scheduleController.create);
+router.put("/:id", authenticate, isAdmin, scheduleController.update);
+router.delete("/:id", authenticate, isAdmin, scheduleController.delete);
+
+export default router;
