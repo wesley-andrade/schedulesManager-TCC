2af49ebd38b7577f90bd67f66509fe7db6be6e2d
Wesley Andrade <wesleyandrade1217@gmail.com>
2025-04-22 20:13:39 -0300

    Adicionando dotenv no middleware para consertar erro de token

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../types";
+import dotenv from "dotenv";
+dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET || "chave_secreta_padrao";
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const authenticate = (
   req: Request,
